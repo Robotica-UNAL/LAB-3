5054dc31b2a5bd490afde500ef70b59335d1fbee
Brian Camilo Valencia <bcvalenciap@unal.edu.co>
2022-01-21 20:31:14 -0800

    Add files via upload

--- LAB3BV.m
+++ LAB3BV.m
@@ -28,7 +28,7 @@ L(6) = Link('revolute','alpha', pi/2,      'a', -28.8,   'd',0,   'offset', 0,  
 %% Definición de la trayectoria espacio cartesiano (espacio de trabajo)
 
 alcanceh = 559 %alcance horizontal del robot mm
-L1 = 223.6;
+L1 = 223.6; %Dimensiones del largo de la figura que recorrerá el EF
 %Grupo #10
 
 
@@ -36,27 +36,29 @@ Xi = 305.52;
 Yi = 0.0;
 Zi = 570.92;
 
-T0=R.fkine([0 0 0 0 0 0])
+T0=R.fkine([0 0 0 0 0 0]) %Inv Kinemathics para Posición incial del robot
 
-
+%Puntos que recorrerá el robot en el espacio de trabajo
 
 p1=transl(Xi,Yi+157.68,Zi-157.68);
 p2=transl(Xi+L1/3,Yi+157.68,Zi-157.68); 
 p3=transl(Xi+L1/3,Yi,Zi);
 p4=transl(Xi,Yi,Zi);
 
+%Trayectorias resultantes de las rutas establecidas
 T_traj = ctraj(T0,p1,20);
 T_traj2 = ctraj(p1,p2,20);
 T_traj3 = ctraj(p2,p3,20);
 T_traj4 = ctraj(p3,p4,20);
 
+%Vector para guardar la trayectoria y posterior graficación
 q_ctraj = R.ikunc(T_traj(:,:,:));
 q_ctraj(21:40,:) = R.ikunc(T_traj2(:,:,:));
 q_ctraj(41:60,:) = R.ikunc(T_traj3(:,:,:));
 q_ctraj(61:80,:) = R.ikunc(T_traj4(:,:,:));
 
 
-
+%Graficación de la trayectoria
 figure
 quiver3(Xi,Yi,Zi,0,400,400);
 hold on
@@ -65,7 +67,7 @@ axis([-1000 1000 -1000 1000 0 1000])
 for i=1:length(q_ctraj)
     punto = R.fkine(q_ctraj(i,:));
     R.plot(q_ctraj(i,:),'workspace',[-300 300 -300 300 -300 300]), 
-    plot3(punto(1,4),punto(2,4),punto(3,4),'r*') %esta linea no la entiendo
+    plot3(punto(1,4),punto(2,4),punto(3,4),'r*') 
     %view(3)
     pause(0.5)
 end
